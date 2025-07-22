9e8fb60265620a97f75b1659d1640cd87eb8e12d
Sajitha94 <sajithasubramani94@gmail.com>
2025-07-22 20:45:54 +0530

    design

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("sa");
+
+let cardGameDiv=document.getElementById("cardGame")
+console.log(cardGameDiv,"ss");
+let gridCards=document.createElement("div")
+gridCards.setAttribute("class", "grid lg:grid-cols-4 grid-cols-3 gap-5 flex-grow px-10")
+gridCards.innerHTML +=`
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+    <div class="card card-body"></div>
+`
+cardGameDiv.append(gridCards)
